9b4bc343e5e609f234fff924496cd81950304afa
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2024-12-23 11:00:00 +0530

    commit

--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import { checkCarAvailability, rentCar } from "../services/rental.service.js"
-import { addCar, getCarById } from "../repositories/car.repo.js"
+import { addCar, getAvailableCars, getCarById } from "../repositories/car.repo.js"
 
 const carRouter = Router()
 
 carRouter.route("/check-available").get(checkCarAvailability)
 carRouter.route("/rent").post(rentCar)
 carRouter.route("/add-car").post(addCar)
-carRouter.route("get-car-by-id").get(getCarById)
+carRouter.route("/get-car-by-id").get(getCarById)
+carRouter.route("/available-cars").get(getAvailableCars)
 
 export default carRouter;
